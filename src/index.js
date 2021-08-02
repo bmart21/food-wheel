6bb6891b0687c77609e3ae3ab45a629fa1d9e623
bmart21 <martinez.brandon.m@gmail.com>
2021-08-02 12:38:11 -0400

    git add -A

--- src/index.js
+++ src/index.js
@@ -1,15 +1,159 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
+  
+function RepeatButton(props) {
+  return (
+    <button 
+      aria-label='Play again.' 
+      id='repeatButton' 
+      onClick={props.onClick}>
+    </button>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }  
+
+  handleClick() { 
+    this._child1.forceUpdateHandler();
+    this._child2.forceUpdateHandler();
+    this._child3.forceUpdateHandler();
+  }
+
+
+  render() {
+    let repeatButton = null
+    repeatButton = <RepeatButton onClick={this.handleClick} />
+
+    return (
+      <div>
+        <div className={`spinner-container`}>
+          <Spinner  ref={(child) => { this._child1 = child; }} timer="1000" />
+          <Spinner  ref={(child) => { this._child2 = child; }} timer="1400" />
+          <Spinner  ref={(child) => { this._child3 = child; }} timer="2200" />
+          <div className="gradient-fade"></div>
+        </div>
+        {repeatButton}          
+      </div>
+    );
+  }
+}  
+  
+class Spinner extends React.Component {  
+  constructor(props){
+    super(props);
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+  };
+
+  forceUpdateHandler(){
+    this.reset();
+  }; 
+
+  reset() {
+    if (this.timer) { 
+      clearInterval(this.timer); 
+    }  
+
+    this.start = this.setStartPosition();
+
+    this.setState({
+      position: this.start,
+      timeRemaining: this.props.timer        
+    });
+
+    this.timer = setInterval(() => {
+      this.tick()
+    }, 100);      
+  }
+
+  state = {
+    position: 0,
+    lastPosition: null
+  }
+  static iconHeight = 188;
+  multiplier = Math.floor(Math.random()*(4-1)+1);
+
+  start = this.setStartPosition();
+  speed = Spinner.iconHeight * this.multiplier;    
+
+  setStartPosition() {
+    return ((Math.floor((Math.random()*9))) * Spinner.iconHeight)*-1;
+  }
+
+  moveBackground() {
+    this.setState({ 
+      position: this.state.position - this.speed,
+      timeRemaining: this.state.timeRemaining - 100
+    })
+  }
+
+  getSymbolFromPosition() {
+    let { position } = this.state;
+    const totalSymbols = 9;
+    const maxPosition = (Spinner.iconHeight * (totalSymbols-1)*-1);
+    let moved = (this.props.timer/100) * this.multiplier
+    let startPosition = this.start;
+    let currentPosition = startPosition;    
+
+    for (let i = 0; i < moved; i++) {              
+      currentPosition -= Spinner.iconHeight;
+
+      if (currentPosition < maxPosition) {
+        currentPosition = 0;
+      }      
+    }
+  }
+
+  tick() {      
+    if (this.state.timeRemaining <= 0) {
+      clearInterval(this.timer);        
+      this.getSymbolFromPosition();    
+
+    } else {
+      this.moveBackground();
+    }      
+  }
+
+  componentDidMount() {
+    clearInterval(this.timer);
+
+    this.setState({
+      position: this.start,
+      timeRemaining: this.props.timer
+    });
+
+    this.timer = setInterval(() => {
+      this.tick()
+    }, 100);
+  }
+
+  render() {
+    let { position, current } = this.state;   
+
+    return (            
+      <div 
+        style={{backgroundPosition: '0px ' + position + 'px'}}
+        className={`icons`}          
+      />
+    )
+  }
+}
+
+function runApp() {
+  ReactDOM.render(
+    <App />,
+    rootElement
+  )
+}
+  
+runApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
